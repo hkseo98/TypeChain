e4e8247f63329c7e38a2047986a655c41605766f
hkseo98 <69430175+hkseo98@users.noreply.github.com>
2021-04-13 18:23:28 +0900

    implement chain

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,37 @@
+"use strict";
 // const name = "hkseo",
 //     age = 24,
 //     gender = "male"
-
+exports.__esModule = true;
 // const sayHi = (name, age, gender?) => { // ?를 파라미터 뒤에 붙이면 옵션이 됨.
 //     console.log(`Hello ${name} you are ${age}, ${gender}`)
 // }
-
 // sayHi(name, age)
-
 // export {}
-
 //-------------------------------
-
 // const sayHi = (name: string, age: number, gender: string): number => { // 타입 지정 가능, 반환 값 설정.
 //     console.log(`Hello ${name} you are ${age}, ${gender}`)
 //     return age;
 // }
-
 // console.log(sayHi("hkseo", 24, "male"))
-
 // export {}
-
 //-------------------------------
-
 // interface Human {
 //     name:string,
 //     age:number,
 //     gender:string
 // }
-
 // const person ={
 //     name:"hkseo",
 //     gender:"male",
 //     age:24
 // }
-
 // const sayHi = (human:Human): number => { // 인터페이스를 통해 타입 지정 가능
 //     console.log(`Hello ${human.name} you are ${human.age}, ${human.gender}`)
 //     return human.age;
 // }
-
 // console.log(person)
-
 // export {}
-
 // class Human {  // 클래스를 사용하면 인터페이스와 달리 컴파일 후에도 보임
 //     public name: string;
 //     public age: number;
@@ -54,94 +42,69 @@
 //         this.gender = gender;
 //     }
 // }
-
 // const hk = new Human("hkseo", 24, "male");
-
 // const sayHi = (human: Human): number => { // 인터페이스를 통해 타입 지정 가능
 //     console.log(`Hello ${human.name} you are ${human.age}, ${human.gender}`)
 //     return human.age;
 // }
-
 // console.log(sayHi(hk))
-
 // export {}
-
 var CryptoJS = require("crypto-js");
-
-
-class Block {
-    public index: number;
-    public hash: string;
-    public previousHash: string;
-    public data: string;
-    public timestamp: number;
-
-    static calculateBlockHash = (
-        index: number,
-        previousHash: string,
-        timestamp: number,
-        data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
-
-    static validateStructure = (aBlock: Block): boolean =>
-        typeof aBlock.index === "number" &&
-        typeof aBlock.hash === "string" &&
-        typeof aBlock.previousHash === "string" &&
-        typeof aBlock.timestamp === "number" &&
-        typeof aBlock.data === "string"
-
-    constructor(index: number, hash: string, previousHash: string, data: string, timestamp: number) {
+var Block = /** @class */ (function () {
+    function Block(index, hash, previousHash, data, timestamp) {
         this.index = index, this.hash = hash,
             this.previousHash = previousHash, this.data = data,
-            this.timestamp = timestamp
+            this.timestamp = timestamp;
     }
-}
-
-const genesisBlock: Block = new Block(0, "20202020202", "", "first block", 12345);
-
-let blockchain: Block[] = [genesisBlock];
-
-const getBlockchain = (): Block[] => blockchain;
-
-const getLatestBlock = (): Block => getBlockchain[blockchain.length - 1];
-
-const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000)
-
-const createNewBlock = (data: string): Block => {
-    const previousBlock: Block = getLatestBlock();
-    const newIndex: number = previousBlock.index + 1;
-    const newTimestamp: number = getNewTimeStamp();
-    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
-    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+    Block.calculateBlockHash = function (index, previousHash, timestamp, data) { return CryptoJS.SHA256(index + previousHash + timestamp + data).toString(); };
+    Block.validateStructure = function (aBlock) {
+        return typeof aBlock.index === "number" &&
+            typeof aBlock.hash === "string" &&
+            typeof aBlock.previousHash === "string" &&
+            typeof aBlock.timestamp === "number" &&
+            typeof aBlock.data === "string";
+    };
+    return Block;
+}());
+var genesisBlock = new Block(0, "20202020202", "", "first block", 12345);
+var blockchain = [genesisBlock];
+var getBlockchain = function () { return blockchain; };
+var getLatestBlock = function () { return getBlockchain[blockchain.length - 1]; };
+var getNewTimeStamp = function () { return Math.round(new Date().getTime() / 1000); };
+var createNewBlock = function (data) {
+    var previousBlock = getLatestBlock();
+    var newIndex = previousBlock.index + 1;
+    var newTimestamp = getNewTimeStamp();
+    var newHash = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    var newBlock = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
     addBlock(newBlock);
     return newBlock;
-}
-
-const getHashforBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data)
-
-const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+};
+var getHashforBlock = function (aBlock) { return Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data); };
+var isBlockValid = function (candidateBlock, previousBlock) {
     if (!Block.validateStructure(candidateBlock)) {
         return false;
-    } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    }
+    else if (previousBlock.index + 1 !== candidateBlock.index) {
         return false;
-    } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    }
+    else if (previousBlock.hash !== candidateBlock.previousHash) {
         return false;
-    } else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
+    }
+    else if (getHashforBlock(candidateBlock) !== candidateBlock.hash) {
         return false;
-    } else {
+    }
+    else {
         return true;
     }
-}
-
-const addBlock = (candidateBlock: Block): void => {
+};
+var addBlock = function (candidateBlock) {
     if (isBlockValid(candidateBlock, getLatestBlock())) {
         blockchain.push(candidateBlock);
     }
-}
-
+};
 createNewBlock("second block");
 createNewBlock("third bye");
 createNewBlock("fourth bye");
-
-console.log(blockchain)
-
-export { };
+console.log(blockchain);
+//# sourceMappingURL=index.js.map
